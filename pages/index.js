a7227152d7981e076f79b1b7fede5c58c9732b68
kev-inc <kevinchanxy@gmail.com>
2021-01-30 15:25:46 +0000

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home({data}) {
+export default function Home() {
     
   return (
     <div className={styles.container}>
-        {data.map((item, index) => (<div>{item.url}</div>))}
       <iframe height='720' width='1280' allowFullScreen={true} sandbox="allow-scripts" src="https://vidoo.streamango.to/e/rxdhss7wvjtt"/>
     </div>
   )
-}
-
-export async function getStaticProps(context) {
-  const res = await fetch(`/api/watch/movie/pokemon-the-movie-volcanion-and-the-mechanical-marvel-2016`)
-  const data = await res.json()
-
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
-
-  return {
-    props: {data}, // will be passed to the page component as props
-  }
 }
